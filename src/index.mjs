4c2f40fcc59781ea147488ff281ece2c93d21fae
skynocover <skynocover@Gmail.com>
2021-12-07 11:56:35 +0800

    update

---
+++ src/index.mjs
@@ -0,0 +1,43 @@
+#!/usr/bin/env node
+
+import 'dotenv/config';
+
+import axios from 'axios';
+import fs from 'fs';
+import fsPromises from 'fs/promises';
+
+const folder = 'locales';
+
+axios.defaults.baseURL = 'https://api.airtable.com';
+axios.defaults.headers.common['Authorization'] = `Bearer ${process.env.I18N_AIRTABLE_API_KEY}`;
+
+(async () => {
+  const { data } = await axios.get(
+    `/v0/${process.env.I18N_AIRTABLE_WORKSPACE}/${process.env.I18N_AIRTABLE_TABLE}`,
+  );
+
+  const { records } = data;
+
+  let languages = [];
+  let config = {};
+
+  if (records.length > 0) {
+    languages = Object.keys(records[0].fields).filter((item) => item !== 'key');
+    for (const lng of languages) {
+      config[lng] = { translation: {} };
+    }
+  }
+
+  for (const record of records) {
+    const fields = record.fields;
+    for (const lng of languages) {
+      config[lng]['translation'][fields.key] = fields[lng];
+    }
+  }
+
+  if (!fs.existsSync(folder)) {
+    await fsPromises.mkdir(folder);
+  }
+
+  await fsPromises.writeFile(`locales/config.json`, JSON.stringify(config, undefined, 2));
+})();
